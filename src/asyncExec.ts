0d48f30ec3928bb55ff33a467e34371c456faae6
heifade <heifade@126.com>
2017-12-22 17:10:37 +0800

    no message

--- src/asyncExec.ts
+++ src/asyncExec.ts
@@ -1,32 +1,29 @@
-import { spawnSync } from "child_process";
+import { spawn } from "child_process";
 
 export async function asyncExec(cmd: string, args?: string[]) {
   return new Promise<string>((resolve, reject) => {
-    let childProcess = spawnSync(cmd, args);
+    let childProcess = spawn(cmd, args);
     let resultMessage = "";
 
-    console.log(resultMessage);
-
-
-    resolve(resultMessage);
+    
 
 
 
-    // childProcess.stdout.on("data", data => {
-    //   resultMessage += "\n" + data;
-    // });
+    childProcess.stdout.on("data", data => {
+      resultMessage += "\n" + data;
+    });
 
-    // childProcess.stderr.on("data", data => {
-    //   resultMessage += "\n" + data;
-    // });
+    childProcess.stderr.on("data", data => {
+      resultMessage += "\n" + data;
+    });
 
-    // childProcess.on("close", code => {
-    //   if (code == 0) {
-    //     console.log(cmd, args, resultMessage);
-    //     resolve(resultMessage);
-    //   } else {
-    //     reject(resultMessage);
-    //   }
-    // });
+    childProcess.on("exit", code => {
+      if (code == 0) {
+        console.log(cmd, args, resultMessage);
+        resolve(resultMessage);
+      } else {
+        reject(resultMessage);
+      }
+    });
   });
 }
