f497b173adddea3b38cbd4ca207bbb0ffd7fef59
heifade <heifade@126.com>
2017-12-22 22:05:55 +0800

    no message

--- src/asyncExec.ts
+++ src/asyncExec.ts
@@ -1,37 +1,26 @@
-import { spawnSync } from "child_process";
+import { spawn } from "child_process";
 
 export async function asyncExec(cmd: string, args?: string[]) {
   return new Promise<string>((resolve, reject) => {
+    let childProcess = spawn(cmd, args, {
+      stdio: ["pipe", "pipe", "pipe"]
+    });
+    let resultMessage = "";
 
-    let returnValue = spawnSync(cmd, args, {encoding: 'utf8'});
-    console.log(returnValue.stdout);
+    childProcess.stdout.on("data", data => {
+      resultMessage += "\n" + data;
+    });
 
-    resolve('1')
+    childProcess.stderr.on("data", data => {
+      resultMessage += "\n" + data;
+    });
 
-
-
-    // let childProcess = spawn(cmd, args, {
-    //   stdio: [
-    //     'pipe', 'pipe', 'pipe'
-    //   ]
-      
-    // });
-    // let resultMessage = "";
-
-    // childProcess.stdout.on("data", data => {
-    //   resultMessage += "\n" + data;
-    // });
-
-    // childProcess.stderr.on("data", data => {
-    //   resultMessage += "\n" + data;
-    // });
-
-    // childProcess.on("close", code => {
-    //   if (code == 0) {
-    //     resolve(resultMessage);
-    //   } else {
-    //     reject(resultMessage);
-    //   }
-    // });
+    childProcess.on("close", code => {
+      if (code == 0) {
+        resolve(resultMessage);
+      } else {
+        reject(resultMessage);
+      }
+    });
   });
 }
