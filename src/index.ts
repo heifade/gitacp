a64d1d77ad54db2f5bad652aa13891af338ef317
heifade <heifade@126.com>
2017-12-23 10:25:32 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,10 @@ let argv = yargs.option("m", {
 
 async function run(commitMessage: string) {
   let resultMsg = await asyncExec("git", ["status", "-s", "-uno"]);
-  (resultMsg.split('\n')).map(msg => {
-    console.log(chalk.blue.bold(resultMsg));
+  resultMsg.split("\n").map(msg => {
+    console.log(chalk.blue.bold(msg));
   });
 
-  
   resultMsg = await asyncExec("git", ["add", "."]);
   console.log(chalk.green.bold(resultMsg));
   resultMsg = await asyncExec("git", ["commit", "-m", `${commitMessage}`]);
