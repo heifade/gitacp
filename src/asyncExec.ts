0ce5762927ef7ea6d696b778b3da941b89e58e9a
heifade <heifade@126.com>
2017-12-23 10:11:23 +0800

    no message

--- src/asyncExec.ts
+++ src/asyncExec.ts
@@ -15,12 +15,7 @@ export async function asyncExec(cmd: string, args?: string[]) {
       resultMessage += "\n" + data;
     });
 
-    process.stdout.on('data', message => {
-      console.log(11, message);
-    });
-    process.on('message', code => {
-      console.log(code);
-    })
+    
     
 
     childProcess.on("close", code => {
