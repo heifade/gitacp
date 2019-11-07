24e8378d16e0b4b56a7ba81133972b49e9fed36d
heifade <heifade@126.com>
2019-11-07 21:25:47 +0800

    调整 bin

--- bin/gitacp.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\gitacp\bin\gitacp" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\gitacp\bin\gitacp" %*
-)
